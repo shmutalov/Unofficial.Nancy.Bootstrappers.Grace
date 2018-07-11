927ee2731b6f8da03f56dc15cf74b7082192e945
shmutalov <shmutalov@gmail.com>
2018-07-11 10:39:19 +0500

    Add project files.

---
+++ Unofficial.Nancy.Bootstrapers.Grace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unofficial.Nancy.Bootstrapers.Grace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
